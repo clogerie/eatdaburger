22aec622aab14e9a9a41c5eb4075338e8c2d0b06
clogerie <66565287+clogerie@users.noreply.github.com>
2021-01-03 14:21:30 -0500

    directory structure, created db, created model, created controller, created orm, set up connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beyond Meat Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Quarter Pounder With Cheese", False);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", FALSE);
